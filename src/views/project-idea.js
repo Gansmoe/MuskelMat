a992025cda3f5733157b013e3e8b45ee0347d455
Isac Gansmoe <isac@gansmoe.se>
2022-04-07 11:37:16 +0200

    Lite mer stylat och lagt till div'ar i main. Även lagt till en logobild.

--- src/views/project-idea.js
+++ src/views/project-idea.js
@@ -3,4 +3,4 @@ import { Page } from "../page.js";
 export const projectIdea = new Page("Productidea");
 
 projectIdea.createElement("h1", "title", "Projektidén");
-projectIdea.createElement("p", "text", "Projektet består i att göra en prototyp-app där man ska kunna få fram en enkel matsedel");
+projectIdea.createElement("p", "text", "Projektet består i att göra en prototyp-app där man ska kunna få fram en veckomatsedel baserad på användarens protein- och kaloribehov. Publika APIer kommer användas för recept i början men om tid finns över vill jag använda egna APIer.");
