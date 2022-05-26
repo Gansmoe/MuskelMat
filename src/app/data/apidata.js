0b3d5943c30fd329021427b628e659fe4831e1a9
Isac Gansmoe <isac@gansmoe.se>
2022-05-26 16:52:19 +0200

    added some more use of context

--- src/app/data/apidata.js
+++ src/app/data/apidata.js
@@ -1,9 +1,9 @@
-const url = "https://localhost:7257/api/recipes/";
+const recipeUrl = "https://localhost:7257/api/recipes/";
 const apiKey = "apiKey=1c2c8e8baba1425a9ce29512cf79c67b";
 
 export async function getRecipesByNutrients() {
     try {
-        const response = await fetch(url);
+        const response = await fetch(recipeUrl);
         const data = await response.json();
         console.log(data);
         return [data, null];
@@ -11,13 +11,12 @@ export async function getRecipesByNutrients() {
     catch (err) {
 
         return [[], err];
-
     }
 };
 
 export async function getDataRecipeInformation(id) {
     try {
-        const response = await fetch(url + id);
+        const response = await fetch(recipeUrl + id);
         const data = await response.json();
         console.log(data);
         return [data, null];
@@ -29,9 +28,20 @@ export async function getDataRecipeInformation(id) {
     }
 };
 
-export async function changeRecipe(id) {
+export async function addRecipe(name, kcal, protein, mealType) {
     try {
-        const response = await fetch(url + id);
+        const response = await fetch(recipeUrl, {
+            method: 'POST',
+            body: JSON.stringify({
+                Name: name,
+                Kcal: kcal,
+                Protein: protein,
+                MealType: mealType
+            }),
+            headers: {
+                'Content-Type': 'application/json; charset=UTF-8',
+            },
+        });
         const data = await response.json();
         console.log(data);
         return [data, null];
